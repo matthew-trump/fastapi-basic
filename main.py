85424417bbc1a3e9a8eb0f918482eff3962a8fb5
matthew-trump <matthew.trump@gmail.com>
2025-12-30 08:24:49 -0800

    adding env variable for cors enablement

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 from fastapi import FastAPI
 import uvicorn
 from pydantic import BaseModel
+from starlette.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class LoginRequest(BaseModel):
     username: str
     password: str
 
 app = FastAPI()
+
+use_cors = os.getenv('USE_CORS', 'False').lower() == 'true'
+print(f"CORS Enabled: {use_cors}")
+
+if use_cors:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 @app.get("/health")
 def read_health():
