cd127d29d104ab3a3c361ff8d886b985830f1768
matthew-trump <matthew.trump@gmail.com>
2025-12-31 09:56:51 -0800

    adding logout test route

--- main.py
+++ main.py
@@ -42,5 +42,9 @@ async def login(request: LoginRequest):
         raise HTTPException(status_code=401, detail="Incorrect username or password")
     return {"status": "ok"}
 
+@app.post("/api/logout")
+async def logout():
+    return {"status": "ok"}
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=3003)
